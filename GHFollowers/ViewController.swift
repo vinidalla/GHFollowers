77d01ce55b3da43b1877da8f66850be4042ccb43
vinicius.vechia <vinicius.vechia@e-unicred.com.br>
2024-01-03 06:33:03 -0300

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Vinícius Dalla Vechia on 03/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+

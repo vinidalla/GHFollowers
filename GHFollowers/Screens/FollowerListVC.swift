a95fd6dd2aa6391e063ee65eb8339245c8a9b638
vinicius.vechia <vinichr@hotmail.com>
2024-01-12 07:05:20 -0300

    configured collectionViewFlowLayout setting spaces, positioning, item sizes etc

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -10,12 +10,49 @@ import UIKit
 class FollowerListVC: UIViewController {
   
   var username: String!
+  var collectionView: UICollectionView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    configureViewController()
+    configureCollectionView()
+    getFollowers()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.setNavigationBarHidden(false, animated: true)
+    navigationController?.navigationBar.tintColor = UIColor.systemGreen
+  }
+  
+  func configureViewController() {
     view.backgroundColor = UIColor.systemBackground
     navigationController?.navigationBar.prefersLargeTitles = true
+  }
+  
+  func configureCollectionView() {
+    collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+    view.addSubview(collectionView)
+    collectionView.backgroundColor = UIColor.systemPink
+    collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+  }
+  
+  func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+    let width = view.bounds.width
+    let padding: CGFloat = 12
+    let minimumItemSpacing: CGFloat = 10
+    //total width - padding in the left and the right - space between pictures
+    let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+    let itemWidth = availableWidth / 3
     
+    let flowLayout = UICollectionViewFlowLayout()
+    flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+    flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+    
+    return flowLayout
+  }
+  
+  func getFollowers() {
     NetworkManager.shared.getFollowers(for: username, page: 1) { result in
       
       switch result {
@@ -27,11 +64,5 @@ class FollowerListVC: UIViewController {
                                         buttonTitle: "Ok")
       }
     }
-  }
-  
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    navigationController?.setNavigationBarHidden(false, animated: true)
-    navigationController?.navigationBar.tintColor = UIColor.systemGreen
   }
 }
